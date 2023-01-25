0b989842431f6b1d4c1d9600a674d1bf2d44a2e8
Harry-Owen <harrison.h23rd@gmail.com>
2023-01-25 21:25:33 +0000

    modified:   crypto.html 	modified:   css/contacts.css 	modified:   css/crypto.css 	modified:   js/crypto.js

--- js/crypto.js
+++ js/crypto.js
@@ -1,8 +1,9 @@
-fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false')
 .then(response => response.json())
 .then(data => {
 data.forEach(crypto => {
 let cryptoContainer = document.getElementById("crypto-container");
+
   // Create a div for each crypto
   let cryptoDiv = document.createElement("div");
   cryptoDiv.id = crypto.id;
@@ -38,7 +39,7 @@ let cryptoContainer = document.getElementById("crypto-container");
           datasets: [{
             label: crypto.name,
             data: dataPoints,
-            backgroundColor: trend === "up" ? "rgba(0, 255, 0, 0.5)" : "rgba(255, 0, 0, 0.5)",
+            backgroundColor: trend === "up" ? "rgba(0, 255, 0, 0.3)" : "rgba(255, 0, 0, 0.3)",
             borderColor: trend === "up" ? "green" : "red"
           }]
         },
@@ -52,4 +53,7 @@ let cryptoContainer = document.getElementById("crypto-container");
       });
     });
 });
-}); 
+});
+
+
+
